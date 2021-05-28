4f78a133c94c78c47a8e44e3647002eaae8af73e
Noon <er4goon@gmail.com>
2021-05-28 11:26:58 +0700

    Initial Project

--- src/main/kotlin/com/arrow/next/ui/glidepool/Bitmap+.kt
+++ src/main/kotlin/com/arrow/next/ui/glidepool/Bitmap+.kt
@@ -4,18 +4,30 @@ import android.graphics.Bitmap
 import android.net.Uri
 
 
-fun Uri.GlideBitmapDecode(width: Int = 200, height: Int = 200): Bitmap {
-    if (width != 200 && height != 200) {
-        return GlideBitmapFactory.decodeFile(this.path, width, height)
-    }
+fun Uri.GlideBitmapDecode(): Bitmap {
     return GlideBitmapFactory.decodeFile(this.path)
 }
 
-fun String.GlideBitmapDecode(width: Int = 200, height: Int = 200): Bitmap {
-    if (width != 200 && height != 200) {
-        return GlideBitmapFactory.decodeFile(this, width, height)
-    }
+fun String.GlideBitmapDecode(): Bitmap {
     return GlideBitmapFactory.decodeFile(this)
+}
+
+
+fun Uri.GlideBitmapDecode(width: Int, height: Int): Bitmap {
+    return if (width == 0 || height == 0) {
+        GlideBitmapFactory.decodeFile(this.path, 200, 200)
+    } else {
+        GlideBitmapFactory.decodeFile(this.path, width, height)
+    }
+
+}
+
+fun String.GlideBitmapDecode(width: Int, height: Int): Bitmap {
+    return if (width == 0 || height == 0) {
+        GlideBitmapFactory.decodeFile(this, 200, 200)
+    } else {
+        GlideBitmapFactory.decodeFile(this, width, height)
+    }
 }
 
 
